3355126e71d54c79340882728942cf167a846796
Hamid Molareza <kitm.hamid@gmail.com>
2020-08-03 18:46:40 +0430

    Fixes: StatusCode

--- FunctionalUtility/ResultUtility/MethodResultBase.cs
+++ FunctionalUtility/ResultUtility/MethodResultBase.cs
@@ -1,8 +1,12 @@
+using Microsoft.AspNetCore.Http;
+
 namespace FunctionalUtility.ResultUtility {
     public abstract class MethodResultBase {
         protected MethodResultBase (bool isSuccess, ResultDetail? detail = null) {
             IsSuccess = isSuccess;
-            Detail = detail ?? new ResultDetail (-1, title: "No Data!", message: "That's all we know.");
+            Detail = detail ?? new ResultDetail (
+                isSuccess ? StatusCodes.Status200OK : StatusCodes.Status500InternalServerError,
+                title: "No Data!", message: "That's all we know.");
         }
 
         public bool IsSuccess { get; }
